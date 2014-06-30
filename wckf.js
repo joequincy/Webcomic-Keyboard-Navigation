b1c2798e16386891e4c70f6eda23801ef71b508b
joequincy <github@pnwebs.com>
2014-06-30 17:54:32 -0600

    Revert "Version 0.4"
    This reverts commit bbc0feed44c76d6f6a52f3f371c4e2997f0c2806.

--- wckf.js
+++ wckf.js
@@ -1,9 +1,9 @@
 /** WcKS Framework */
-wckf = {
+ksf = {
 	'bind':function(shortcuts){
 		function closure(e){
 			var cf = document.activeElement.nodeName;
-			var element = (e.keyCode==wckf.direction.l)?shortcuts.prev:(e.keyCode==wckf.direction.r)?shortcuts.next:null;
+			var element = (e.keyCode==ksf.direction.l)?shortcuts.prev:(e.keyCode==ksf.direction.r)?shortcuts.next:null;
 			if((e.keyCode==37||e.keyCode==39)&&!(cf=="INPUT"||cf=="TEXTAREA")){
 				e.preventDefault();
 				$(element)[0].click();
@@ -20,16 +20,16 @@ wckf = {
 		storage.get('sites',function(items){
 			var stuff = items.sites;
 			for(var p in stuff){
-				var reg = new RegExp(wckf.escape(p)+"$");
+				var reg = new RegExp(ksf.escape(p)+"$");
 				if(reg.test(document.domain)){
 					if(!(stuff[p].hasOwnProperty("path")) || document.location.pathname.indexOf(stuff[p]['path'])==0){
 						storage.get('direction',function(items){
 							if(items.direction=="r"){
-								wckf.direction.l = 39;
-								wckf.direction.r = 37;
+								ksf.direction.l = 39;
+								ksf.direction.r = 37;
 							}
 						});
-						wckf.bind(stuff[p]['shortcuts']);
+						ksf.bind(stuff[p]['shortcuts']);
 					}
 				}
 			}
@@ -43,4 +43,3 @@ wckf = {
 		'r':39
 	}
 }
-wckf.apply();
